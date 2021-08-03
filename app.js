06fb14ff505dd002ac6015aa46865118e312d169
kamran0812 <34595994+kamran0812@users.noreply.github.com>
2021-08-03 16:03:37 +0530

    Update

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(80);
+console.log("Hello World")
